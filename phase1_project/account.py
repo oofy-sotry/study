69e13e596531bdba6d2bd5ed82638761ca630c7a
oofy <oofysotry@gmail.com>
2026-02-22 20:08:45 +0900

    withdrwa 작성

--- phase1_project/account.py
+++ phase1_project/account.py
@@ -8,3 +8,10 @@ class Account:
         if amount <= 0:
             raise ValueError("0보다 커야 합니다")
         self.balance += amount
+
+    def withdrwa(self, amount):
+            if amount > self.balance:
+                raise ValueError("잔액이 부족합니다")
+            self.balance -= amount
+
+
